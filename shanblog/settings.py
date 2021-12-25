d3fad8d3d3dbd640728adb4e40de080e4bfc8a01
ShankarSwarup <sankarrocky58@gmail.com>
2021-12-25 19:27:20 +0530

    first commit

--- shanblog/settings.py
+++ shanblog/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
