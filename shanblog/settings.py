8875f71a970ca9498ea5edf983caf010ea8fb30c
ShankarSwarup <sankarrocky58@gmail.com>
2022-01-16 11:32:07 +0530

    profile page

--- shanblog/settings.py
+++ shanblog/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'static')
 ]
 
 
